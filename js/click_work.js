15c96606f157aa02bfcd9d18f22ef6533e92d1e7
ks830111 <47165060+ks830111@users.noreply.github.com>
2019-03-04 08:00:26 +0900

    Add files via upload

---
+++ js/click_work.js
@@ -0,0 +1,61 @@
+$(function(){
+    // header
+
+    // 축약식
+    // $('.menu_ul a').click(function(){
+    //     $('.menu_ul li').removeClass('select');
+    //     $(this).parent().addClass('select');
+    // });
+
+    $('.menu_li1').click(function(){
+        $('.menu_ul li').removeClass('select');
+        $(this).addClass('select');
+    });
+
+    $('.menu_li2').click(function(){
+        $('.menu_ul li').removeClass('select');
+        $(this).addClass('select');
+    });
+
+    $('.menu_li3').click(function(){
+        $('.menu_ul li').removeClass('select');
+        $(this).addClass('select');
+    });
+    
+    // work
+    $('.menu0').click(function(){
+        $('.content_menu a').removeClass('select');
+        $(this).addClass('select');
+    });
+
+    $('.menu1').click(function(){
+        $('.content_menu a').removeClass('select');
+        $(this).addClass('select');
+    });
+
+    $('.menu2').click(function(){
+        $('.content_menu a').removeClass('select');
+        $(this).addClass('select');
+    });
+
+    $('.menu3').click(function(){
+        $('.content_menu a').removeClass('select');
+        $(this).addClass('select');
+    });
+
+    $('.menu4').click(function(){
+        $('.content_menu a').removeClass('select');
+        $(this).addClass('select');
+    });
+
+    $('.menu5').click(function(){
+        $('.content_menu a').removeClass('select');
+        $(this).addClass('select');
+    });
+
+    $('.menu6').click(function(){
+        $('.content_menu a').removeClass('select');
+        $(this).addClass('select');
+    });
+
+});
---
+++ js/menu_work.js
@@ -0,0 +1,44 @@
+$(function(){
+    $('.content_menu > a.menu0').click(function(){
+        $('.works-image_box > ul').removeClass('show')
+        $('.works-image_box .webSite_main').addClass('show')
+        return false;
+    })
+
+    $('.content_menu > a.menu1').click(function(){
+        $('.works-image_box > ul').removeClass('show')
+        $('.works-image_box .graphic-poster_main').addClass('show')
+        return false;
+    })
+
+    $('.content_menu > a.menu2').click(function(){
+        $('.works-image_box > ul').removeClass('show')
+        $('.works-image_box .icon_main').addClass('show')
+        return false;
+    })
+
+    $('.content_menu > a.menu3').click(function(){
+        $('.works-image_box > ul').removeClass('show')
+        $('.works-image_box .banner_main').addClass('show')
+        return false;
+    })
+
+    $('.content_menu > a.menu4').click(function(){
+        $('.works-image_box > ul').removeClass('show')
+        $('.works-image_box .mainPage_main').addClass('show')
+        return false;
+    })
+
+    $('.content_menu > a.menu5').click(function(){
+        $('.works-image_box > ul').removeClass('show')
+        $('.works-image_box .mobile_main').addClass('show')
+        return false;
+    })
+
+    $('.content_menu > a.menu6').click(function(){
+        $('.works-image_box > ul').removeClass('show')
+        $('.works-image_box .project_main').addClass('show')
+        return false;
+    })
+    
+});
